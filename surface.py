322c8098c1cfd0c35e175ee19962c58aa0815e4d
RichardRivero <Richard_armando@hotmail.com>
2021-01-29 23:25:05 -0400

    variable color

--- surface.py
+++ surface.py
@@ -12,10 +12,13 @@ surface = pygame.display.set_mode((width, height)) # superficies esto define la 
 pygame.display.set_caption("Hola mundo!!") # este es el titulo de la ventana 
 
 #----------------------------------------------------------------
-# colores
+# colores usando la clase Color
 # el resive argumentos en RGB 
 red = pygame.Color(255,0,0)  # nota .Color la c es en mayuscula
 
+# o por medio de tuplas
+my_color = (253,58,94)   # esta tupla genera un rgb tambien 
+#---------------------------------------------------------------
 
 while True:       # este while estara a la escucha de todo los eventos 
     for event in pygame.event.get():
@@ -24,6 +27,7 @@ while True:       # este while estara a la escucha de todo los eventos
             sys.exit()
 #-------------------------------
 # dentro del bucle se coloca el color 
-    surface.fill(red)
-    pygame.display.update()
+# tanto una tupla como la libreria color van dentro de fill()
+    surface.fill(my_color)           #  .fill() resive un parameto y es el que pinta la pantalla
+    pygame.display.update()     # el .updata() es para que la pantalla se actualice y se ve el cambio 
 
