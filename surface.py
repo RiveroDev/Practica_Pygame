e531cf31f5f161b68de00334ea7e3e4a7ce0b083
RichardRivero <Richard_armando@hotmail.com>
2021-01-29 23:18:11 -0400

    nodifcando color pantalla

--- surface.py
+++ surface.py
@@ -11,9 +11,19 @@ surface = pygame.display.set_mode((width, height)) # superficies esto define la 
 
 pygame.display.set_caption("Hola mundo!!") # este es el titulo de la ventana 
 
+#----------------------------------------------------------------
+# colores
+# el resive argumentos en RGB 
+red = pygame.Color(255,0,0)  # nota .Color la c es en mayuscula
+
+
 while True:       # este while estara a la escucha de todo los eventos 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # el evento QUIT la ventana cerrara
             pygame.quit()
             sys.exit()
+#-------------------------------
+# dentro del bucle se coloca el color 
+    surface.fill(red)
+    pygame.display.update()
 
