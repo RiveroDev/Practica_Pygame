3959757cba54918ea04b414c0529ce75efa8c57e
RichardRivero <Richard_armando@hotmail.com>
2021-01-29 23:10:26 -0400

    modificando comentarios y anadiendo titulo de ventana

--- surface.py
+++ surface.py
@@ -5,11 +5,15 @@ pygame.init()
 
 width = 400
 height = 500
-surface = pygame.display.set_mode((width, height)) # superficies
+surface = pygame.display.set_mode((width, height)) # superficies esto define la ventana 
 
-while True:
+# almacenamos la superfici en una variable 'surface'
+
+pygame.display.set_caption("Hola mundo!!") # este es el titulo de la ventana 
+
+while True:       # este while estara a la escucha de todo los eventos 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # el evento QUIT la ventana cerrara
             pygame.quit()
             sys.exit()
 
